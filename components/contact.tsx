c65f7a6652e3e530fc37f95ac31ae5ae98506673
Antonov-S <anto1nov1@gmail.com>
2023-11-18 02:12:30 +0200

    Finishing Contact section

--- components/contact.tsx
+++ components/contact.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import React from "react";
+import toast from "react-hot-toast";
 import { motion } from "framer-motion";
-import { FaPaperPlane } from "react-icons/fa";
 
 import SectionHeading from "./section-heading";
 import { useSectionInView } from "@/lib/hooks";
+import { sendEmail } from "@/actions/sendEmail";
+import SubmitBtn from "./submit-btn";
 
 export default function Contact() {
   const { ref } = useSectionInView("Contact");
@@ -32,18 +34,28 @@ export default function Contact() {
 
       <p className="text-gray-700 -mt-6 dark:text-white/80">
         Please contact me directly at{" "}
-        <a className="underline" href="mailto:anto1nov1@gmail.com">
-          anto1nov1@gmail.com
+        <a className="underline" href="mailto:example@gmail.com">
+          example@gmail.com
         </a>{" "}
         or through this form.
       </p>
 
-      <form className="mt-10 flex flex-col">
-        <input type="email" placeholder="Your email" className="h-14 px-4 rounded-lg borderBlack" />
-        <textarea name="" placeholder="Your message" id="" className="h-52 my-3 rounded-lg borderBlack p-4"></textarea>
-        <button type="submit" className="group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 dark:bg-white dark:bg-opacity-10 disabled:scale-100 disabled:bg-opacity-65">
-          Submit <FaPaperPlane className="text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1" />{" "}
-        </button>
+      <form
+        className="mt-10 flex flex-col dark:text-black"
+        action={async formData => {
+          const { data, error } = await sendEmail(formData);
+
+          if (error) {
+            toast.error(error);
+            return;
+          }
+
+          toast.success("Email sent successfully!");
+        }}
+      >
+        <input className="h-14 px-4 rounded-lg borderBlack dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none" name="senderEmail" type="email" required maxLength={500} placeholder="Your email" />
+        <textarea className="h-52 my-3 rounded-lg borderBlack p-4 dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none" name="message" placeholder="Your message" required maxLength={5000} />
+        <SubmitBtn />
       </form>
     </motion.section>
   );
